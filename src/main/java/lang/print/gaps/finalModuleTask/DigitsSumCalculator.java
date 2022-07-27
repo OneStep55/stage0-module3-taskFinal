d86c147f3ccd02a78fd5920e52a4c7bc317bbae7
OneStep55 <vnmcfn@gmail.com>
2022-07-28 01:45:22 +0600

    completed tasks

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        sum = number % 10 + (number /= 10) % 10 + (number /= 10) % 10 + (number /= 10);
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int revertedNum = 0;
+        revertedNum += (number % 10) * 100 + ((number /= 10)% 10) * 10 + (number /= 10) ;
+
+        System.out.println(revertedNum);
     }
+
+
 }
